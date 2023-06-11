820a62ae1b0d3de58a0fb128185ea690d50806ef
Gordon Murray <gordon@gordonmurray.com>
2023-06-11 19:02:43 +0100

    debezium packer

--- packer/files/start_registry.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# This starts the apicura schema registry
-sudo java -Dregistry.kafka.common.bootstrap.servers=[KAFKA_BROKERS] -jar apicurio-registry-storage-kafkasql-2.4.1.Final-runner.jar
