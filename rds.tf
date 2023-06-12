19f39c19e300cf1f71314c16bdcd191ac4476e70
Gordon Murray <gordon@gordonmurray.com>
2023-06-12 20:45:41 +0100

    rds mariadb version

--- rds.tf
+++ rds.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "default" {
   allocated_storage            = 20
   storage_type                 = "gp2"
   engine                       = "mariadb"
-  engine_version               = "10.6.7"
+  engine_version               = "10.6.13"
   instance_class               = "db.t4g.micro"
   db_subnet_group_name         = aws_db_subnet_group.default.name
   username                     = "Admin"
