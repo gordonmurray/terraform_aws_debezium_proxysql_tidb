1f371c4196ceaf29c2d86db0298576f651fd69c3
Gordon Murray <gordon@gordonmurray.com>
2023-07-02 12:08:46 +0100

    security group rules

--- security_group_rules_proxysql.tf
+++ security_group_rules_proxysql.tf
@@ -1,13 +1,3 @@
-resource "aws_security_group_rule" "proxysql_0" {
-  type              = "egress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["${var.my_ip_address}/32"]
-  security_group_id = aws_security_group.proxysql.id
-  description       = "SSH egress"
-}
-
 resource "aws_security_group_rule" "proxysql_ssh" {
   type              = "ingress"
   from_port         = 22
@@ -16,4 +6,34 @@ resource "aws_security_group_rule" "proxysql_ssh" {
   cidr_blocks       = ["${var.my_ip_address}/32"]
   security_group_id = aws_security_group.proxysql.id
   description       = "SSH ingress"
+}
+
+resource "aws_security_group_rule" "proxysql_rds" {
+  type              = "egress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.rds.id
+  security_group_id = aws_security_group.proxysql.id
+  description       = "ProxySQL egress to RDS"
+}
+
+resource "aws_security_group_rule" "proxysql_tidb" {
+  type              = "egress"
+  from_port         = 4000
+  to_port           = 4000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.proxysql.id
+  description       = "ProxySQL egress to TiDB"
+}
+
+resource "aws_security_group_rule" "proxysql_open" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.my_ip_address}/32"]
+  security_group_id = aws_security_group.proxysql.id
+  description       = "ProxySQL ingress for my IP address"
 }
